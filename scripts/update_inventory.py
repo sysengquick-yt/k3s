278126de4d456771a9e9df3bcdcd00753d9d30dc
John Ratliff <john@technoplaza.net>
2024-03-22 14:24:00 +0000

    remove k3s_cp_server group

--- scripts/update_inventory.py
+++ scripts/update_inventory.py
@@ -29,7 +29,6 @@ def main():
                 "k3s": {
                     "children": {
                         "servers": {
-                            "children": {"k3s_cp_server": {"hosts": {}}},
                             "hosts": {},
                         },
                         "workers": {"hosts": {}},
@@ -54,11 +53,6 @@ def main():
         worker["name"]: {"ansible_host": worker["ip"]}
         for worker in config["cluster_nodes"]["workers"]
     }
-
-    k3s_cp_server = config["cluster_nodes"]["servers"][0]
-    inventory["all"]["children"]["k3s"]["children"]["servers"]["children"][
-        "k3s_cp_server"
-    ]["hosts"] = {k3s_cp_server["name"]: {}}
 
     inventory_path = f"{path_prefix}/inventory.yaml"
 
