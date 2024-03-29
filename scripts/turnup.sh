7d05f3a28942a5f2f8df1e6a965d052266f0f2d4
John Ratliff <john@technoplaza.net>
2024-03-29 19:21:48 +0000

    fix turnup script

--- scripts/turnup.sh
+++ scripts/turnup.sh
@@ -16,5 +16,5 @@ time ansible-playbook sysengquick.k3s.deploy_rancher
 time kubectl -n cattle-system rollout status deploy/rancher
 
 kubectl get nodes -o wide
-kubectl get nodes --all-namespaces -o wide
+kubectl get pods --all-namespaces -o wide
 kubectl -n cattle-system get deploy rancher
