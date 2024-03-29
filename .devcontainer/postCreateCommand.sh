48479b1ec92b977170d5da8a8eebfcda074bb7fc
John Ratliff <john@technoplaza.net>
2024-03-29 13:47:44 +0000

    update postCreateCommand
    Handle mounted folder permissions better

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
 
-# fix docker volume ownership
-sudo chown -R vscode:vscode ~/.kube
-sudo chown -R vscode:vscode /bash_history
+UID=$(id -u)
 
-# ensure k3s vault password file exists
-mkdir -p /private/ansible/vault/sysengquick 2>/dev/null
-touch -a /private/ansible/vault/sysengquick/k3s
+# fix docker created volume permissions
+for folder in ~/.kube /bash_history /private
+do
+    test $(stat -c %u $folder) -ne $UID \
+        && sudo chown vscode:vscode $folder
+done
+
+VAULT_DIR="/private/ansible-vault"
+VAULT_FILE="${VAULT_DIR}/sysengquick-k3s"
+
+mkdir -p ${VAULT_DIR} 2>/dev/null \
+    && touch -a ${VAULT_FILE} \
+    && chmod 600 ${VAULT_FILE}
